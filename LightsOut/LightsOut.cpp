a7c1731f93fbbc91bf06eeec94436060593ef3d9
stavilam <stavilam@coventry.ac.uk>
2019-08-31 05:51:11 +0100

    changed arrays to vectors (dynamic game size), added checks for bad inputs

--- LightsOut/LightsOut.cpp
+++ LightsOut/LightsOut.cpp
@@ -3,19 +3,24 @@
 #include <iostream>
 #include <array>
 #include <random>
+#include <vector>
 #include "LightsOut.h"
 
 
-void printMap(unsigned int gameMap[][5], unsigned const int gameSize)
+void printMap(std::vector< std::vector<int> >& gameMap, int& gameSize)
 {
 	std::cout << "   ";
-	for (unsigned int i = 0; i < gameSize; i++)
+	for (int i = 0; i < gameSize; i++)
 		std::cout << i << " ";
-	std::cout << "\n -------------- \n";
-	for (unsigned int i = 0; i < gameSize; i++)
+	std::cout << "\n ";
+	for (int i = 0; i < gameSize; i++)
+		std::cout << "---";
+	std::cout << " \n";
+
+	for (int i = 0; i < gameSize; i++)
 	{
 		std::cout << i << "| ";
-		for (unsigned int j = 0; j < gameSize; j++)
+		for (int j = 0; j < gameSize; j++)
 		{
 			std::cout << gameMap[i][j] << " ";
 		}
@@ -24,11 +29,11 @@ void printMap(unsigned int gameMap[][5], unsigned const int gameSize)
 	std::cout << std::endl;
 }
 
-bool winCheck(unsigned int gameMap[][5], unsigned const int gameSize)
+bool winCheck(std::vector< std::vector<int> >& gameMap, int& gameSize)
 {
-	for (unsigned int i = 0; i < gameSize; i++)
+	for (int i = 0; i < gameSize; i++)
 	{
-		for (unsigned int j = 0; j < gameSize; j++)
+		for (int j = 0; j < gameSize; j++)
 		{
 			if (gameMap[i][j] == 0)
 				return false;
@@ -37,95 +42,134 @@ bool winCheck(unsigned int gameMap[][5], unsigned const int gameSize)
 	return true;
 }
 
-bool checkRange(unsigned int a, unsigned const int gameSize)
+bool checkRange(int a, int& gameSize)
 {
 	if (!(a >= 0 && a < gameSize))
 		return false;
 	return true;
 }
 
-unsigned int lightSwitch(unsigned int gameMap[][5], unsigned const int gameSize, unsigned int a, unsigned b)
+void lightSwitch(std::vector< std::vector<int> >& gameMap, int& gameSize, int const &x, int const &y)
 {
-	if (checkRange(a, gameSize) && checkRange(b, gameSize))
+	if (checkRange(x, gameSize) && checkRange(y, gameSize))
 	{
-		gameMap[a][b] += 1;
-		if (gameMap[a][b] > 1)
+		gameMap[x][y] += 1;
+		if (gameMap[x][y] > 1)
 		{
-			if (gameMap[a][b] % 2 == 0)
-				gameMap[a][b] = 0;
+			if (gameMap[x][y] % 2 == 0)
+				gameMap[x][y] = 0;
 			else
-				gameMap[a][b] = 1;
+				gameMap[x][y] = 1;
 		}
 	}
-	return gameMap[gameSize][gameSize];
 }
 
-unsigned int lightPress(unsigned int gameMap[][5], unsigned const int gameSize, unsigned int x, unsigned int y)
+void lightPress(std::vector< std::vector<int> >& gameMap, int& gameSize, int const &x, int const &y)
 {
 	//If the coordinates given are out of range
 	if ( !checkRange(x, gameSize) || !checkRange(y, gameSize) )
 	{
 		std::cout << "Out of range. \n";
-		return 0;
+	}
+	else 
+	{
+		lightSwitch(gameMap, gameSize, x, y);
+		lightSwitch(gameMap, gameSize, x + 1, y);
+		lightSwitch(gameMap, gameSize, x - 1, y);
+		lightSwitch(gameMap, gameSize, x, y + 1);
+		lightSwitch(gameMap, gameSize, x, y - 1);
 	}
 	
-	gameMap[gameSize][gameSize] = lightSwitch(gameMap, gameSize, x, y);
-	gameMap[gameSize][gameSize] = lightSwitch(gameMap, gameSize, x + 1, y);
-	gameMap[gameSize][gameSize] = lightSwitch(gameMap, gameSize, x - 1, y);
-	gameMap[gameSize][gameSize] = lightSwitch(gameMap, gameSize, x, y + 1);
-	gameMap[gameSize][gameSize] = lightSwitch(gameMap, gameSize, x, y - 1);
-	return gameMap[gameSize][gameSize];
 }
 
-unsigned int generateMap(unsigned int gameMap[][5], unsigned const int gameSize, unsigned int difficulty)
+void generateMap(std::vector< std::vector<int> >& gameMap, int& gameSize, int& difficulty)
 {
 	if (difficulty < 1)
 		difficulty = 1;
 	else if (difficulty > 100)
 		difficulty = 100;
+
 	std::random_device random; //Will be used to obtain a seed for the random number engine
 	std::mt19937 gen(random()); //Standard mersenne_twister_engine seeded with random()
 	std::uniform_int_distribution<> dis(0, gameSize - 1); // dis(gen)
-	for (unsigned int i = 0; i < gameSize; i++)
+
+	for (int i = 0; i < gameSize; i++)
 	{
-		for (unsigned int j = 0; j < gameSize; j++)
+		std::vector<int> temp;
+		for (int j = 0; j < gameSize; j++)
 		{
-			gameMap[i][j] = 1;
+			temp.push_back(1);
 		}
+		gameMap.push_back(temp);
 	}
 	while (winCheck(gameMap, gameSize) == 1)
 	{
-		for (unsigned int ct = 0; ct < difficulty; ct++)
+		for (int ct = 0; ct < difficulty; ct++)
 		{
 			int row = dis(gen), col = dis(gen);
-			gameMap[gameSize][gameSize] = lightPress(gameMap, gameSize, row, col);
+			lightPress(gameMap, gameSize, row, col);
 			std::cout << "[" << row << "," << col << "]\n";
 		}
 	}
 
 	std::cout << "Lights shuffled " << difficulty << " time(s):\n" << "----------------------\n";
 	printMap(gameMap, gameSize);
-
-	return gameMap[gameSize][gameSize];
 }
 
 int main()
 {
-	unsigned const int gameSize = 5;
-	unsigned int gameMap[gameSize][gameSize];
-	int difficulty;
-	std::cout << "Please select amount of shuffles (1 to 100): \n", std::cin >> difficulty;
-	gameMap[gameSize][gameSize] = generateMap(gameMap, gameSize, difficulty);
+	int gameSize, difficulty;
+	std::vector< std::vector<int> > gameMap;
+	
+	//Game Size
+	std::cout << "Input game size: ", std::cin >> gameSize;
+	while (!std::cin) {
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Game size must be a number from 1 to 9. Try again: ", std::cin >> gameSize;
+		while ((gameSize < 1) || (gameSize > 9))
+		{
+			std::cout << "Game size must be a number from 1 to 9. Try again: ", std::cin >> gameSize;
+		}
+	}
 
-	unsigned int userInput = 1, x, y;
+	//Shuffles
+	std::cout << "Please select amount of shuffles (1 to 100): \n", std::cin >> difficulty;
+	while (!std::cin) {
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Shuffles amount must be a number from 1 to 100. Try again: ", std::cin >> difficulty;
+		while ((difficulty < 1) || (difficulty > 100))
+		{
+			std::cout << "Shuffles amount must be a number from 1 to 100. Try again: ", std::cin >> difficulty;
+		}
+	}
+
+	
+	generateMap(gameMap, gameSize, difficulty); //Map generation
+
+	//Game loop
+	int userInput = 1, x, y;
 	while (userInput != 9)
 	{
-		std::cout << "Please input 2 positive numbers as coordinates x (row) and y (col) -- or 9 to close: ", std::cin >> userInput;
+		//User input coordinates
+		std::cout << "Please input 2 positive numbers as coordinates x (row) and y (col) -- or 9 to close: \n", std::cin >> userInput;
+		while (!std::cin) {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Coordinates must be integers. Try again: ", std::cin >> userInput;
+
+			while ((userInput < 0) || (userInput > (gameSize - 1) * 10 + (gameSize - 1)))
+			{
+				std::cout << "Inputted coordinates are out of range. Try again: ", std::cin >> userInput;
+			}
+		}
 		if (userInput == 9)
 			return 0;
-		x = static_cast<unsigned int>(userInput / 10);
-		y = static_cast<unsigned int>(userInput % 10);	
-		gameMap[gameSize][gameSize] = lightPress(gameMap, gameSize, x, y);
+		x = static_cast<int>(userInput / 10);
+		y = static_cast<int>(userInput % 10);	
+
+		lightPress(gameMap, gameSize, x, y);
 		printMap(gameMap, gameSize);
 		
 		if (winCheck(gameMap, gameSize) == 1)
